66e55ba53058584555714282e1d8103f0bbfedf7
FritzHeider <fritz.heider@students.makeschool.com>
2020-04-28 10:36:20 -0700

    Initial Commit

---
+++ Habitual/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Habitual
+//
+//  Created by Fritz Heider on 4/28/20.
+//  Copyright © 2020 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
